05d5606e881c31ec4fcee29a7405ec3a2c3d7c34
Luiz Balloti <lhballoti@gmail.com>
2012-09-30 23:11:28 -0300

    Moved MAX7219 register map to source file

--- Blink01/max7219.c
+++ Blink01/max7219.c
@@ -17,7 +17,22 @@
 #error "MAX7219 slave select port not defined"
 #endif
 
-void MAX7219_Send( uint8_t addr, uint8_t data ) {
+#define MAX7219_ADDR_NOOP 0x00
+#define MAX7219_ADDR_DIG0 0x01
+#define MAX7219_ADDR_DIG1 0x02
+#define MAX7219_ADDR_DIG2 0x03
+#define MAX7219_ADDR_DIG3 0x04
+#define MAX7219_ADDR_DIG4 0x05
+#define MAX7219_ADDR_DIG5 0x06
+#define MAX7219_ADDR_DIG6 0x07
+#define MAX7219_ADDR_DIG7 0x08
+#define MAX7219_ADDR_DECM 0x09
+#define MAX7219_ADDR_INTN 0x0A
+#define MAX7219_ADDR_SCLM 0x0B
+#define MAX7219_ADDR_SHDN 0x0C
+#define MAX7219_ADDR_TEST 0x0F
+
+static void MAX7219_Send( uint8_t addr, uint8_t data ) {
 	MAX7219_SS_PORT &= ~_BV( MAX7219_SS_PIN );
 	spi_transfer( addr );
 	spi_transfer( data );
